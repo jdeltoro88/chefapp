1632207709f2516164c10d5c2cbee3f2c84b9b10
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2024-01-20 13:51:25 -0700

    misssing dep module

--- chef/src/App.js
+++ chef/src/App.js
@@ -1,25 +1,17 @@
 
 import './App.css';
 import React from "react;"
+import {BrowserRouter, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        
-        </a>
-      </header>
+    <BrowserRouter>
+    <div className="app">
+      <Route component={Home} path="/" exact />
     </div>
+      </BrowserRouter>
+    
   );
 }
 
